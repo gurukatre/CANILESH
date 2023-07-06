41087bb2cf316c423986e37cc9fb3e1a460a60ee
gurukatre <phpgkguru@gmail.com>
2023-07-06 18:59:59 +0200

    1 binaries uploaded

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { Navbar } from '../components/Navbar';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -9,7 +10,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <div>Hello World</div>
+      <div>
+        <Image
+          src="/images/profile.jpg"
+          height={200}
+          width={300}
+          alt="Nilesh"
+          style={{ 'border-radius': '100%' }}
+        />
+      </div>
     </div>
   );
 }
